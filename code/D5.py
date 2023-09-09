ec4507b1048eb3e9ccec12217ce3b5700e0844df
samcarmen <carmen.samkahman@gmail.com>
2023-09-09 20:54:03 +0800

    Update file names

--- code/D5.py
+++ code/D5.py
@@ -1,6 +1,6 @@
 ######### Nice String Part 1 #########
 def check_vowels(string):
-    vowel = ['a', 'e', 'i', 'o', 'u']           
+    vowel = ["a", "e", "i", "o", "u"]
     vowel_num = 0
 
     for char in string:
@@ -13,14 +13,14 @@ def check_vowels(string):
 
 
 def check_appear_twice(string):
-    for i in range(len(string)-1):
-        if string[i] == string[i+1]:
+    for i in range(len(string) - 1):
+        if string[i] == string[i + 1]:
             return True
     return False
 
 
 def check_bad_strings(string):
-    bad_letters = ['ab', 'cd', 'pq', 'xy'] 
+    bad_letters = ["ab", "cd", "pq", "xy"]
     for bad in bad_letters:
         if bad in string:
             return False
@@ -29,26 +29,27 @@ def check_bad_strings(string):
 
 ######### Nice String Part 2 #########
 def check_overlaps(string):
-    for i in range(len(string)-1):
-        pair = string[i] + string[i+1]
-        for j in range(i+2, len(string)-1):
-            if pair == string[j] + string[j+1]:
+    for i in range(len(string) - 1):
+        pair = string[i] + string[i + 1]
+        for j in range(i + 2, len(string) - 1):
+            if pair == string[j] + string[j + 1]:
                 return True
     return False
 
+
 def check_repeat_between_character(string):
-    for i in range(len(string)-2):
-        if string[i] == string[i+2]:
-            return True   
+    for i in range(len(string) - 2):
+        if string[i] == string[i + 2]:
+            return True
+
 
 if __name__ == "__main__":
     nice_string = 0
-    with open('.//input//D5_Input.txt') as data: 
+    with open(".//input//D5_Input.txt") as data:
         for line in data:
             string = line.strip()
 
-        # string = 'qjhvhtzxzqqjkmpb'
+            # string = 'qjhvhtzxzqqjkmpb'
             if check_overlaps(string) and check_repeat_between_character(string):
                 nice_string += 1
     print(nice_string)
-
--- code/D6.py
+++ code/D6.py
@@ -32,11 +32,39 @@ def execute_instruction(grid, instruction):
                 grid[x][y] ^= 1
 
 
+def adjust_lights_brightness(grid, instructions):
+    for instruction in instructions:
+        action, start_x, start_y, end_x, end_y = parse_instruction(instruction)
+        
+        for x in range(start_x, end_x + 1):
+            for y in range(start_y, end_y + 1):
+                if action == "turn on":
+                    grid[x][y] += 1
+                elif action == "turn off":
+                    grid[x][y] = max(0, grid[x][y] - 1)
+                elif action == "toggle":
+                    grid[x][y] += 2
+    return grid
+
+
+def total_brightness_after_instructions(instructions, grid_size=1000):
+    # Initialize the grid with a brightness of 0 for each light
+    grid = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
+    
+    # Adjust the lights' brightness based on the instructions
+    grid = adjust_lights_brightness(grid, instructions)
+    
+    # Calculate the total brightness
+    total_brightness = sum(sum(row) for row in grid)
+    
+    return total_brightness
+
+
 def main(instructions):
     # Initialize a 1000x1000 grid with all lights off
     grid = [[0] * 1000 for _ in range(1000)]
 
-    with open('./input/D6_Input.txt') as data:
+    with open('./input/D6.txt') as data:
         for instruction in data:
             instructions.append(instruction)
 
@@ -45,6 +73,8 @@ def main(instructions):
     for instruction in instructions:
         execute_instruction(grid, instruction)
 
+    result = total_brightness_after_instructions(instructions)
+    return result
     # Count the lights that are on
     return sum(sum(row) for row in grid)
 
