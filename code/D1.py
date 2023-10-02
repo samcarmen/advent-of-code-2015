996f4a26ffe1d1baea8a5da453e9952ac1539f16
samcarmen <carmen.samkahman@gmail.com>
2023-10-02 16:23:32 +0800

    refactor D1 solution

--- code/D1.py
+++ code/D1.py
@@ -1,17 +1,34 @@
-current_floor = 0
-position = 1
+def find_floor(instructions: str) -> int:
+    floor = 0
+    for char in instructions:
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+    return floor
 
-with open(".//input//D1_Input.txt") as data:
-    for line in data:
-        for ele in line:
-            if ele == "(":
-                current_floor += 1
-                if current_floor < 0:
-                    break
-                position += 1
-            else:
-                current_floor -= 1
-                if current_floor < 0:
-                    break
-                position += 1
-print(position)
+
+def find_basement_entry(instructions: str) -> int:
+    floor = 0
+    for position, char in enumerate(instructions, 1):
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+
+        if floor == -1:
+            return position
+    return None  # Santa never enters the basement with the given instructions
+
+
+if __name__ == "__main__":
+    # First part
+    with open("./input/D1.txt", "r") as file:
+        instructions = file.readline().strip()
+
+    floor = find_floor(instructions)
+    print(f"Santa is going to floor: {floor}")
+
+    # Second part
+    basement_entry = find_basement_entry(instructions)
+    print(f"Santa is entering basement at: {basement_entry}")
