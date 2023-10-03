b9c97d4f8f8f6c6909860ada662f7a85e31bad81
samcarmen <carmen.samkahman@gmail.com>
2023-10-03 13:07:49 +0800

    refactor D2

--- code/D2.py
+++ code/D2.py
@@ -1,21 +1,26 @@
-input_list, number, total = [], [], 0
+def paper_required(l, w, h):
+    sides = [l * w, w * h, h * l]
+    surface_area = sum([2 * s for s in sides])
+    return surface_area + min(sides)
 
-with open('./input/D2_Input.txt') as file_input:
-    for each in file_input:
-        each = each.rstrip().split('x')
-        converted = [int(i) for i in each]
-        [l, w, h] = sorted(converted)
-        subtotal = 2*l*w + 2*w*h + 2*l*h + l*w
-        total += subtotal
 
-####### Ribbon #######
-with open('.//input//D2_Input.txt') as file_input:
-    total = 0
-    for each in file_input:
-        each = each.rstrip().split('x')
-        converted = [int(i) for i in each]
-        [l, w, h] = sorted(converted)
-        subtotal = l + l + w + w + (l*w*h)
-        total += subtotal
+def ribbon_required(l, w, h):
+    sides = sorted([l, w, h])
+    return 2 * (sides[0] + sides[1]) + l * w * h
 
-print(total)
+
+def total_paper_required(box_list):
+    total_paper = 0
+    total_ribbon = 0
+    for box in box_list:
+        l, w, h = map(int, box.split("x"))
+        total_paper += paper_required(l, w, h)
+        total_ribbon += ribbon_required(l, w, h)
+    return total_paper, total_ribbon
+
+
+if __name__ == "__main__":
+    with open("./input/D2.txt") as file:
+        dimensions = file.readlines()
+
+    print(f"Total paper and ribbon: {total_paper_required(dimensions)}")
