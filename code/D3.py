bfc804cd65a3d1c9f0c34f709b69150de7b6f372
samcarmen <carmen.samkahman@gmail.com>
2023-10-03 14:51:40 +0800

    Refactor D3

--- code/D3.py
+++ code/D3.py
@@ -1,45 +1,49 @@
-directions = []
-with open('.//input//input_d3.txt') as data:
-    for line in data:
-        for char in line:
-            directions.append(char)
+def num_houses(directions):
+    # Initialize Santa's starting position and the set of visited houses
+    x, y = 0, 0
+    visited_houses = {(x, y)}
 
-visited = set()
-santaX, santaY = 0, 0
-roboX, roboY = 0, 0
+    # Map of directions to their corresponding coordinate changes
+    moves = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
 
-visited.add((0, 0)) # the initial starting position
-turn = 1
+    for direction in directions:
+        dx, dy = moves[direction]
+        x += dx
+        y += dy
+        visited_houses.add((x, y))
 
-for direction in directions:
-    if direction == "^":
-        if turn % 2 != 0:
-            santaY += 1
-            visited.add((santaX, santaY))
-        else:
-            roboY += 1
-            visited.add((roboX, roboY))
-    elif direction == "v":
-        if turn % 2 != 0:
-            santaY -= 1
-            visited.add((santaX, santaY))
-        else:
-            roboY -= 1
-            visited.add((roboX, roboY))
-    elif direction == ">":
-        if turn % 2 != 0:
-            santaX += 1
-            visited.add((santaX, santaY))
-        else:
-            roboX += 1
-            visited.add((roboX, roboY))
-    elif direction == "<":
-        if turn % 2 != 0:
-            santaX -= 1
-            visited.add((santaX, santaY))
-        else:
-            roboX -= 1
-            visited.add((roboX, roboY))
-    turn += 1
+    return len(visited_houses)
 
-print(len(visited))
+
+def num_houses_with_robo(directions):
+    santa_x, santa_y = 0, 0
+    robo_x, robo_y = 0, 0
+    visited_houses = {(santa_x, santa_y)}
+
+    moves = {
+        "^": (0, 1),
+        "v": (0, -1),
+        "<": (-1, 0),
+        ">": (1, 0),
+    }
+
+    for idx, direction in enumerate(directions):
+        dx, dy = moves[direction]
+        if idx % 2 == 0:
+            santa_x += dx
+            santa_y += dy
+            visited_houses.add((santa_x, santa_y))
+        else:
+            robo_x += dx
+            robo_y += dy
+            visited_houses.add((robo_x, robo_y))
+
+    return len(visited_houses)
+
+
+if __name__ == "__main__":
+    with open("./input/D3.txt", "r") as file:
+        directions = file.readline()
+
+    print(num_houses(directions))
+    print(num_houses_with_robo(directions))
