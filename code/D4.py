d20eedcfb63aa19cfac95f6a6a3ed88158ff072c
samcarmen <carmen.samkahman@gmail.com>
2023-10-05 17:52:45 +0800

    Refactor D4

--- code/D4.py
+++ code/D4.py
@@ -1,12 +1,21 @@
 import hashlib
 
-key = 'bgvyzdsv'
-hexa = hashlib.md5(key.encode())
-num = 0
 
-while hexa.hexdigest()[0:6] != '000000':
-    new_key = key + str(num)
-    hexa = hashlib.md5(new_key.encode())
-    num += 1
+def find_lowest_number_for_md5(secret_key, prefix="00000"):
+    num = 0
+    while True:
+        data = f"{secret_key}{num}".encode()
+        result = hashlib.md5(data).hexdigest()
+        if result.startswith(prefix):
+            return num
+        num += 1
 
-print(num-1)
+
+if __name__ == "__main__":
+    secret_key = "ckczppom"
+
+    # Part 1
+    print(find_lowest_number_for_md5(secret_key))
+
+    # Part 2
+    print(find_lowest_number_for_md5(secret_key, "000000"))
