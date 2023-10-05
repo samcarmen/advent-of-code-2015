080b677c91ce59f2fe083349779543175133312f
samcarmen <carmen.samkahman@gmail.com>
2023-10-05 18:25:06 +0800

    Refactor D5

--- code/D5.py
+++ code/D5.py
@@ -1,33 +1,27 @@
-######### Nice String Part 1 #########
-def check_vowels(string):
-    vowel = ["a", "e", "i", "o", "u"]
-    vowel_num = 0
-
-    for char in string:
-        if char in vowel:
-            vowel_num += 1
-
-    if vowel_num >= 3:
-        return True
-    return False
+def vowel_counter(string):
+    vowel = "aeiou"
+    vowel_counter = 0
+    for letter in list(string):
+        if letter in vowel:
+            vowel_counter += 1
+    return vowel_counter
 
 
-def check_appear_twice(string):
+def letter_appears_twice(string):
     for i in range(len(string) - 1):
         if string[i] == string[i + 1]:
             return True
     return False
 
 
-def check_bad_strings(string):
+def has_bad_strings(string):
     bad_letters = ["ab", "cd", "pq", "xy"]
-    for bad in bad_letters:
-        if bad in string:
-            return False
-    return True
+    for letter in bad_letters:
+        if letter in string:
+            return True
+    return False
 
 
-######### Nice String Part 2 #########
 def check_overlaps(string):
     for i in range(len(string) - 1):
         pair = string[i] + string[i + 1]
@@ -44,12 +38,20 @@ def check_repeat_between_character(string):
 
 
 if __name__ == "__main__":
-    nice_string = 0
-    with open(".//input//D5_Input.txt") as data:
-        for line in data:
-            string = line.strip()
+    with open("./input/D5.txt", "r") as file:
+        strings = file.readlines()
 
-            # string = 'qjhvhtzxzqqjkmpb'
-            if check_overlaps(string) and check_repeat_between_character(string):
+    # Part 1
+    nice_string = 0
+    for string in strings:
+        if not has_bad_strings(string):
+            if vowel_counter(string) >= 3 and letter_appears_twice(string):
                 nice_string += 1
+    print(nice_string)
+
+    # Part 2
+    nice_string = 0
+    for string in strings:
+        if check_overlaps(string) and check_repeat_between_character(string):
+            nice_string += 1
     print(nice_string)
