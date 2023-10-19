41c52ea2a8d8423a74899e868b088575d0122598
samcarmen <carmen.samkahman@gmail.com>
2023-10-19 16:01:53 +0800

    Update D7 solution

--- code/D7.py
+++ code/D7.py
@@ -1,78 +1,88 @@
-def get_value(wire_signals, cmd):
-    # If it's a number, return its integer value
-    if cmd.isdigit():
-        return int(cmd)
+def get_signal(wire_id, circuit, cache):
+    # If the wire's signal is already cached, return it
+    if wire_id in cache:
+        return cache[wire_id]
 
-    # If the command is a known signal, return its value
-    if cmd in wire_signals:
-        return wire_signals[cmd]
+    # If the wire's signal is numeric (e.g., "123"),
+    # return the parsed integer value
+    if wire_id.isdigit():
+        return int(wire_id)
 
-    return None
+    # Get the instruction for the wire
+    instruction = circuit[wire_id]
 
+    # Parse the instruction
+    parts = instruction.split()
 
-def evaluate(wires, wire_signals):
-    while wires:
-        for cmd in list(wires.keys()):
-            if "AND" in cmd:
-                x, _, y, _, z = cmd.split()
-                val_x = get_value(wire_signals, x)
-                val_y = get_value(wire_signals, y)
-                if val_x is not None and val_y is not None:
-                    wire_signals[z] = val_x & val_y
-                    del wires[cmd]
-            elif "OR" in cmd:
-                x, _, y, _, z = cmd.split()
-                val_x = get_value(wire_signals, x)
-                val_y = get_value(wire_signals, y)
-                if val_x is not None and val_y is not None:
-                    wire_signals[z] = val_x | val_y
-                    del wires[cmd]
-            elif "LSHIFT" in cmd:
-                x, _, n, _, z = cmd.split()
-                val_x = get_value(wire_signals, x)
-                if val_x is not None:
-                    wire_signals[z] = val_x << int(n)
-                    del wires[cmd]
-            elif "RSHIFT" in cmd:
-                x, _, n, _, z = cmd.split()
-                val_x = get_value(wire_signals, x)
-                if val_x is not None:
-                    wire_signals[z] = val_x >> int(n)
-                    del wires[cmd]
-            elif "NOT" in cmd:
-                _, x, _, z = cmd.split()
-                val_x = get_value(wire_signals, x)
-                if val_x is not None:
-                    wire_signals[z] = 65535 - val_x
-                    del wires[cmd]
-            else:
-                _, z = cmd.split(" -> ")
-                val_x = get_value(wire_signals, z)
-                if val_x is None:
-                    x = cmd.split(" -> ")[0]
-                    val_x = get_value(wire_signals, x)
-                    if val_x is not None:
-                        wire_signals[z] = val_x
-                        del wires[cmd]
+    if len(parts) == 1:
+        # Simple assignment without an operator (e.g., "1674 -> b")
+        signal = get_signal(parts[0], circuit, cache)
+    elif len(parts) == 2:
+        # NOT operation (e.g., "NOT ac -> ad")
+        _, x = parts[0], parts[1]
+        signal = ~get_signal(x, circuit, cache) & 0xFFFF
+    else:
+        # Binary operations (e.g., "x AND y -> z" or "x RSHIFT 1 -> y")
+        x = parts[0]
+        op = parts[1]
+        y = parts[2]
 
-    return wire_signals["a"]
+        if op == "AND":
+            signal = get_signal(x, circuit, cache) & get_signal(
+                y,
+                circuit,
+                cache,
+            )
+        elif op == "OR":
+            signal = get_signal(x, circuit, cache) | get_signal(
+                y,
+                circuit,
+                cache,
+            )
+        elif op == "LSHIFT":
+            amount = int(y)
+            signal = (get_signal(x, circuit, cache) << amount) & 0xFFFF
+        elif op == "RSHIFT":
+            amount = int(y)
+            signal = (get_signal(x, circuit, cache) >> amount) & 0xFFFF
+
+    # Cache the computed signal for the wire
+    cache[wire_id] = signal
+    return signal
 
 
-def main():
-    wires = {}
-    wire_signals = {}
-    instructions = []
+if __name__ == "__main__":
+    # Read the instructions from the input file
+    with open("./input/D7.txt", "r") as file:
+        instructions = file.read().splitlines()
 
-    with open("./input/D7.txt") as data:
-        for instruction in data:
-            instructions.append(instruction)
+    # Create a dictionary to store the circuit instructions
+    circuit = {}
 
-    for line in instructions:
-        # Extract the command and destination wire from each line
-        cmd, wire = line.split(" -> ")
-        wires[cmd] = wire
+    # Populate the circuit dictionary with the provided instructions
+    for instruction in instructions:
+        parts = instruction.split(" -> ")
+        circuit[parts[1].strip()] = parts[0]
 
-    return evaluate(wires, wire_signals)
+    # Create a cache to store computed signals
+    signal_cache = {}
 
+    # Calculate the signal for wire 'a'
+    result_signal = get_signal("a", circuit, signal_cache)
 
-print(main())
+    print("Signal on wire 'a':", result_signal)
+
+    # Store the original signal on wire 'a'
+    original_signal_a = result_signal
+
+    # Update the circuit dictionary to override wire 'b' 
+    # with the original signal on 'a'
+    circuit["b"] = str(original_signal_a)
+
+    # Create a cache to store computed signals
+    signal_cache = {}
+
+    # Calculate the new signal for wire 'a' with the updated circuit
+    new_signal_a = get_signal("a", circuit, signal_cache)
+
+    print("New signal on wire 'a':", new_signal_a)
